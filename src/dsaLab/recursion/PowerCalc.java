36e28ee0b4494856c61f980f39b0b2401599acde
nikhil-gaur457 <nikhil.gaur_cs19@gla.ac.in>
2021-02-20 12:18:35 +0530

    calculate value of exponential function using recursion method

---
+++ src/dsaLab/recursion/PowerCalc.java
@@ -0,0 +1,35 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Nikhil gaur (nikhil-gaur457)
+ *   Date: 20-02-2021
+ *   Time: 12:10
+ *   File: PowerCalc.java
+ */
+
+package dsaLab.recursion;
+
+import java.util.Scanner;
+
+public class PowerCalc {
+    public static void main(String[] args) {
+        // class to print the value exponential function
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the base value : ");
+        int base = scanner.nextInt();
+        System.out.print("Enter the exponent value : ");
+        int exponent = scanner.nextInt();
+        scanner.close();
+        if (base < 0 || exponent < 0) {
+            System.err.println("The base and the exponent must be positive!");
+        } else {
+            System.out.println(base + "^" + exponent + " = " + calculatePower(base, exponent));
+        }
+    }
+
+    // method to calculate the expontent function used in main class
+    private static int calculatePower(int base, int exponent) {
+        if (exponent == 1) {
+            return base;
+        }
+        return calculate(base, exponent - 1);
+    }
+}
