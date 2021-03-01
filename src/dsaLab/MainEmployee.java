17610f8d5c96c3e86eba52b7820d6ac700d63820
nikhil-gaur457 <nikhil.gaur_cs19@gla.ac.in>
2021-03-01 11:44:22 +0530

    Execution class for employee

---
+++ src/dsaLab/MainEmployee.java
@@ -0,0 +1,27 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Nikhil gaur (nikhil-gaur457)
+ *   Date: 01-03-2021
+ *   Time: 10:35
+ *   File: MainEmployee.java
+ */
+
+package dsaLab;
+
+import java.util.ArrayList;
+
+public class MainEmployee {
+    public static void main(String[] args) {
+        ArrayList<Employee> arrayList = new ArrayList<>();
+        arrayList.add(new Employee(12, "Nik", "SDE", 90000));
+        arrayList.add(new Employee(13, "alok", "SDE3", 150000));
+        arrayList.add(new Employee(14, "kelly", "SDE2", 120000));
+        arrayList.add(new Employee(15, "eve", "ASDE", 45000));
+        arrayList.add(new Employee(16, "adam", "SDE", 90000));
+
+        DetailsOfEmployee details = new DetailsOfEmployee();
+        for (Employee employee : arrayList) {
+            details.enqueue(new NodeEmployee(employee));
+        }
+        details.display();
+    }
+}
