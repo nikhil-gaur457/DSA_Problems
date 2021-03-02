402015558e4f7b7c7bb6811bc647795268d88ac5
nikhil-gaur457 <nikhil.gaur_cs19@gla.ac.in>
2021-03-02 20:49:36 +0530

    Node class for queue using linked list

---
+++ src/dsaLab/queue/linkedListQueue/QNode.java
@@ -0,0 +1,18 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Nikhil gaur (nikhil-gaur457)
+ *   Date: 02-03-2021
+ *   Time: 19:59
+ *   File: QNode.java
+ */
+
+package dsaLab.queue.linkedListQueue;
+
+public class QNode {
+    int data;
+    QNode next;
+
+    public QNode(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
