cd751ac652143b08580acad92b60a2ce327e2a65
nikhil-gaur457 <nikhil.gaur_cs19@gla.ac.in>
2021-03-04 20:28:03 +0530

    Node class for doubly linked list

---
+++ src/dsaLab/LinkedList/doublyLL/DoublyLLNode.java
@@ -0,0 +1,18 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Nikhil gaur (nikhil-gaur457)
+ *   Date: 04-03-2021
+ *   Time: 19:44
+ *   File: DoublyLLNode.java
+ */
+
+package dsaLab.LinkedList.doublyLL;
+
+public class DoublyLLNode {
+    int data;
+    DoublyLLNode next;
+    DoublyLLNode previous;
+
+    public DoublyLLNode(int d) {
+        data = d;
+    }
+}
