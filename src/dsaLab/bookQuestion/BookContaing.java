0d1b99f0027f53c65b34e19f9766e3ce1deafd45
nikhil-gaur457 <nikhil.gaur_cs19@gla.ac.in>
2021-03-08 19:57:06 +0530

    book class for input data

---
+++ src/dsaLab/bookQuestion/BookContaing.java
@@ -0,0 +1,29 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Nikhil gaur (nikhil-gaur457)
+ *   Date: 08-03-2021
+ *   Time: 18:32
+ *   File: BookContaing.java
+ */
+
+package dsaLab.bookQuestion;
+
+public class BookContaing {
+    private int price;
+    private String bookName;
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+}
