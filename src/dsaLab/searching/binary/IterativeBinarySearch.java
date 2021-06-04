eb2e55c6be2890524d0121ce8708409747de63f1
nikhil-gaur457 <nikhil.gaur_cs19@gla.ac.in>
2021-06-05 02:06:48 +0530

    binary search using iteration in searching.

--- src/dsaLab/searching/binary/IterativeBinarySearch.java
+++ src/dsaLab/searching/binary/IterativeBinarySearch.java
@@ -8,28 +8,10 @@
 package dsaLab.searching.binary;
 
 public class IterativeBinarySearch {
-    int binarySearch(int arr[], int x) {
-        int l = 0;
-        int r = arr.length-1;
-        while (l <= r) {
-            int m = l+ (r-1)/2;
-            if (arr[m] == x) {
-                return m;
-            }
-            if (arr[m] < x) {
-                l = m+1;
-            }
-            else {
-                r = m-1;
-            }
-        }
-        return -1;
-    }
-
     public static void main(String[] args) {
         IterativeBinarySearch ibs = new IterativeBinarySearch();
-        int[] arr = {2,3,4,10,40};
-        int n= arr.length;
+        int[] arr = {2, 3, 4, 10, 40};
+        int n = arr.length;
         int x = 10;
         int result = ibs.binarySearch(arr, x);
         if (result == -1) {
@@ -37,5 +19,22 @@ public class IterativeBinarySearch {
         } else {
             System.out.println("Element found at index " + result);
         }
+    }
+
+    int binarySearch(int[] arr, int x) {
+        int l = 0;
+        int r = arr.length - 1;
+        while (l <= r) {
+            int m = l + (r - 1) / 2;
+            if (arr[m] == x) {
+                return m;
+            }
+            if (arr[m] < x) {
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+        return -1;
     }
 }
