e927c841e5fc54e4d7b2fd669764e2ca7781de2b
nikhil-gaur457 <nikhil.gaur_cs19@gla.ac.in>
2021-06-05 01:19:14 +0530

    Linear searching algorithm in searching

---
+++ src/dsaLab/searching/LinearSearch.java
@@ -0,0 +1,35 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Nikhil gaur (nikhil-gaur457)
+ *   Date: 05-06-2021
+ *   Time: 01:04
+ *   File: LinearSearch.java
+ */
+
+package dsaLab.searching;
+
+import java.util.Scanner;
+
+public class LinearSearch {
+    public static int search(int[] arr, int x) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == x) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] arr = {2, 3, 4, 10, 40};
+        System.out.println("Enter the element to search : ");
+        int x = scanner.nextInt();
+        int result = search(arr, x);
+        if (result == -1) {
+            System.out.println("Element is not present at index");
+        } else {
+            System.out.println("Element is present at index " + result);
+        }
+    }
+}
