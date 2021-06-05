91b53217442324083021f4ec693ab1e44b56402f
nikhil-gaur457 <nikhil.gaur_cs19@gla.ac.in>
2021-06-05 23:30:07 +0530

    bubble sort algorithm

---
+++ src/dsaLab/sorting/BubbleSort.java
@@ -0,0 +1,29 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Nikhil gaur (nikhil-gaur457)
+ *   Date: 05-06-2021
+ *   Time: 23:28
+ *   File: BubbleSort.java
+ */
+
+package dsaLab.sorting;
+
+public class BubbleSort {
+    public void bubbleSort(int[] arr) {
+        int lengthOfArray = arr.length;
+        for (int i = 0; i < lengthOfArray - 1; i++) {
+            for (int j = 0; j < lengthOfArray - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public void printSortedArray(int[] arr) {
+        for (int j : arr) {
+            System.out.print(j + " ");
+        }
+    }
+}
---
+++ src/dsaLab/sorting/BubbleSortMain.java
@@ -0,0 +1,18 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Nikhil gaur (nikhil-gaur457)
+ *   Date: 05-06-2021
+ *   Time: 23:29
+ *   File: BubbleSortMain.java
+ */
+
+package dsaLab.sorting;
+
+public class BubbleSortMain {
+    public static void main(String[] args) {
+        BubbleSort bs = new BubbleSort();
+        int[] arr = {10, 6, 4, 2, 15, 9, 40};
+        bs.bubbleSort(arr);
+        System.out.print("Sorted array is : ");
+        bs.printSortedArray(arr);
+    }
+}
