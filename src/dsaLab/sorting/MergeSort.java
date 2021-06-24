2f66f6072367e4658cb193f9c0cdf249edba6757
nikhil-gaur457 <nikhil.gaur_cs19@gla.ac.in>
2021-06-24 22:04:44 +0530

    merge sort algorithm.

---
+++ src/dsaLab/sorting/MergeSort.java
@@ -0,0 +1,73 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Nikhil gaur (nikhil-gaur457)
+ *   Date: 24-06-2021
+ *   Time: 21:59
+ *   File: MergeSort.java
+ */
+
+package dsaLab.sorting;
+
+public class MergeSort {
+    static void printArray(int[] arr) {
+        int n = arr.length;
+        for (int j : arr) {
+            System.out.print(j + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {12, 11, 13, 5, 6, 7};
+
+        System.out.println("Given Array: ");
+        printArray(arr);
+
+        MergeSort ob = new MergeSort();
+        ob.sort(arr, 0, arr.length - 1);
+
+        System.out.println("Sorted array : ");
+        printArray(arr);
+    }
+
+    void merge(int[] arr, int l, int m, int r) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+        if (n1 >= 0) System.arraycopy(arr, l, L, 0, n1);
+        for (int j = 0; j < n2; ++j) {
+            R[j] = arr[m + 1 + j];
+        }
+        int i = 0, j = 0;
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    void sort(int[] arr, int l, int r) {
+        if (l < r) {
+            int m = l + (r - l) / 2;
+            sort(arr, l, m);
+            sort(arr, m + 1, r);
+            merge(arr, l, m, r);
+        }
+    }
+}
